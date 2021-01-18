109a8a6a2c7edcc5fc6265523ea026c7da3d4a84
mliu098 <75713937+mliu098@users.noreply.github.com>
2021-01-18 15:18:41 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -5,8 +5,8 @@
  data$dateTime <- as.POSIXct(paste(data$Date,data$Time),format = "%d/%m/%Y %H:%M:%S")
  data <- data[(data$dateTime>="2007-02-01") & (data$dateTime < "2007-02-03"),]
  png("plot3.png")
- plot(x=data$dateTime,y=data$Global_active_power)
- plot(x=data$dateTime, y = data$Sub_metering_1,type = "l",xlab = "",ylab = "Energy sub metering",main = "",col = "black")
- points(x=data$dateTime, y = data$Sub_metering_2,type = "l",xlab = "",ylab = "Energy sub metering",main = "",col = "red")
- points(x=data$dateTime, y = data$Sub_metering_3,type = "l",xlab = "",ylab = "Energy sub metering",main = "",col = "blue")
+ plot(data$dateTime,data$Global_active_power)
+ plot(data$dateTime, data$Sub_metering_1,type = "l",xlab = "",ylab = "Energy sub metering",main = "",col = "black")
+ points(data$dateTime, data$Sub_metering_2,type = "l",xlab = "",ylab = "Energy sub metering",main = "",col = "red")
+ points(data$dateTime, data$Sub_metering_3,type = "l",xlab = "",ylab = "Energy sub metering",main = "",col = "blue")
  dev.off() 
